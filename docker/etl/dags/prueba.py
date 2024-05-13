71a5cf62fda68a925e94e75cc7950b575709637a
jarodriguezf <jarodriguezf123@gmail.com>
2024-05-13 18:07:31 +0200

    prueba dag - init

--- docker/etl/dags/prueba.py
+++ docker/etl/dags/prueba.py
@@ -1,10 +1,14 @@
 from airflow.models import DAG
 from airflow.operators.dummy_operator import DummyOperator
+from airflow.operators.python_operator import PythonOperator
 from airflow.utils import dates
 
 default_args={
     'start_date':dates.days_ago(1)
 }
+
+def hello_world():
+    print('Hello World!')
 
 with DAG(
     'prueba_dag',
@@ -12,6 +16,7 @@ with DAG(
     schedule_interval =None
 ) as dag:
     t1 = DummyOperator(task_id='start')
-    t2 = DummyOperator(task_id='end')
+    t2 = PythonOperator(task_id='print_hello', python_callable=hello_world)
+    t3 = DummyOperator(task_id='end')
 
-t1 >> t2
+t1 >> t2 >> t3
