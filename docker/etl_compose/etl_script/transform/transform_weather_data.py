d54d4d07d537a62b8c08c176c1c383324b68e6dd
jarodriguezf <jarodriguezf123@gmail.com>
2024-05-16 11:07:33 +0200

    create function casting to int

--- docker/etl_compose/etl_script/transform/transform_weather_data.py
+++ docker/etl_compose/etl_script/transform/transform_weather_data.py
@@ -23,6 +23,7 @@ def drop_weather_desc(df_copy):
 def convert_to_celsius(df_copy):
     # Convertimos el grado de las variables referentes a las temperaturas a Celsius (por defecto Kelvin)
     df_copy[['temp','temp_min','temp_max']] = df_copy[['temp','temp_min','temp_max']].apply(lambda x: x - 273.15)
+    df_copy[['temp','temp_min','temp_max']] = df_copy[['temp','temp_min','temp_max']].apply(lambda x: round(x, 1))
     logging.info('Conversión de Kelvin a Celsius realizada.')
     return df_copy
 
@@ -41,14 +42,22 @@ def rename_dt_iso_column(df_copy):
     return df_copy
 
 
+def float_to_int(df_copy):
+    # Casteamos variables float a enteros (necesarias dadas 0.0)
+    columns = ['rain_1h','rain_3h','snow_3h']
+    df_copy[columns] = df_copy[columns].astype(int)
+    return df_copy
+
+
 def run_transform_weather_task(df_weather_data):
     """
         Cargamos el dataframe en bruto y procesamos a través de las funciones correspondientes:
             - format_dt_iso: Parseo de string a datetime.
             - drop_weather_desc: Eliminamos variable con descripcion larga (no necesaria).
-            - convert_to_celsius: Convertimos las temperaturas dadas en grados Kelvin a Celsius.
+            - convert_to_celsius: Convertimos las temperaturas dadas en grados Kelvin a Celsius, conservando 1 decimal.
             - convert_to_kmh: Convertimos la velocidad del viento dado en m/s a km/h.
             - rename_dt_iso_column: Renombramos la variable dt_iso (actúa como id).
+            - float_to_int: Casteamos variables a int (necesarias dado valores 0)
     Args:
         df_weather_data (DataFrame): Dataset en bruto (original)
 
@@ -62,6 +71,7 @@ def run_transform_weather_task(df_weather_data):
         df_copy=convert_to_celsius(df_copy)
         df_copy=convert_to_kmh(df_copy)
         df_copy = rename_dt_iso_column(df_copy)
+        df_copy=float_to_int(df_copy)
         return df_copy
     except Exception:
         logging.error('Fallo al intentar transformar el dataframe.')
