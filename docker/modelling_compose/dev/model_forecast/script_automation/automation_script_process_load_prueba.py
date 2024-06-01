26c6d7a437608c05bd31946b92cd9e333e2f0481
jarodriguezf <jarodriguezf123@gmail.com>
2024-06-01 16:44:29 +0200

    ajustar funciones correspondientes a nuevos datos

--- docker/modelling_compose/dev/model_forecast/script_automation/automation_script_process_load_prueba.py
+++ docker/modelling_compose/dev/model_forecast/script_automation/automation_script_process_load_prueba.py
@@ -106,100 +106,16 @@ def logarithm_generation_waste(df_copy):
     return df_copy
 
 
-# generation_fossil_brown_coal_lignite, estratificar en dos categorias 0 y 1.
-def stratify_generation_fossil_brown_coal_lignite(df_copy):
-    """ Si los valores de la variable son mayores que 200, el valor se modifica a 1.
-        En caso contrario, se mantienen en 0.
-    Args:
-        df_copy (Dataframe): copia de dataframe sin modificar valores
-
-    Returns:
-        Dataframe: dataframe modificado
-    """
-    df_copy['generation_fossil_brown_coal_lignite']=df_copy['generation_fossil_brown_coal_lignite'].apply(lambda x: 1 if x >= 200 else 0)
-    return df_copy
-
-
-# Funcion que extrae los grupos de generation_biomass
-def transform_generation_biomass_gmm(filtered_df):
-    data = filtered_df['generation_biomass'].values
-    data = data.reshape(-1,1)
-
-    # entrenar
-    gmm = GaussianMixture(n_components=3, n_init=10)
-    gmm.fit(data)
-
-    # predecir probabilidades
-    values = gmm.predict_proba(data).round(3)
-    values = np.argmax(values, axis=1)
-
-    filtered_df['generation_biomass'] = values
-    return filtered_df
-
-
-
-def gmm_generation_other(df_copy):
-    """Convertir valores de variable a cluster según multiples modos, mediante gaussian mixture.
-
-    Args:
-        df_copy (DataFrame): dataframe si modificar
-
-    Returns:
-        DataFrame: dataframe con valores modificado
-    """
-    # extraer columna
-    data = df_copy['generation_other'].values
-    data = data.reshape(-1,1)
-
-    # entrenar
-    gmm = GaussianMixture(n_components=3, n_init=10)
-    gmm.fit(data)
-
-    # predecir probabilidades
-    values = gmm.predict_proba(data).round(3)
-    values = np.argmax(values, axis=1)
-
-    df_copy['generation_other'] = values
-    return df_copy
-
-
-def gmm_generation_other_renewable(df_copy):
-    """Convertir valores de variable a cluster según multiples modos, mediante gaussian mixture.
-
-    Args:
-        df_copy (DataFrame): dataframe si modificar
-
-    Returns:
-        DataFrame: dataframe con valores modificado
-    """
-    # extraer columna
-    data = df_copy['generation_other_renewable'].values
-    data = data.reshape(-1,1)
-
-    # entrenar
-    gmm = GaussianMixture(n_components=2, n_init=10)
-    gmm.fit(data)
-
-    # predecir probabilidades
-    values = gmm.predict_proba(data).round(3)
-    values = np.argmax(values, axis=1)
-
-    df_copy['generation_other_renewable'] = values
-    return df_copy
-
-
-
 def run_automation_process_load_data_prueba(df):
     try:
         df_copy = df.copy()
 
         df_copy = df_copy[['generation_hydro_pumped_storage_consumption','generation_solar',
-                    'generation_fossil_gas','generation_wind_onshore',
-                    'generation_fossil_oil','generation_hydro_water_reservoir',
-                    'generation_hydro_run_of_river_and_poundage','generation_nuclear',
-                    'generation_fossil_hard_coal','price_actual','generation_waste',
-                    'generation_fossil_brown_coal_lignite','generation_biomass',
-                    'generation_other','generation_other_renewable', 'total_load_actual']]
+                   'generation_fossil_gas','generation_wind_onshore',
+                   'generation_fossil_oil','generation_hydro_water_reservoir',
+                   'generation_hydro_run_of_river_and_poundage','generation_nuclear',
+                   'generation_fossil_hard_coal','price_actual','generation_waste',
+                   'total_load_actual']]
         
         df_copy=stratify_generation_hydro_pumped_storage_consumption(df_copy)
         df_copy = stratify_generation_solar(df_copy)   
@@ -210,9 +126,6 @@ def run_automation_process_load_data_prueba(df):
         distance_transform = distance_transform_generation_fossil_hard_coal(df_copy)
         df_copy = distance_transform.kmeans_transform()
         df_copy = logarithm_generation_waste(df_copy)
-        df_copy = stratify_generation_fossil_brown_coal_lignite(df_copy)
-        df_copy =  gmm_generation_other(df_copy)
-        df_copy =  gmm_generation_other_renewable(df_copy)
         return df_copy
     except Exception as e:
         print('Error, al procesar los datos',e)
